8b1984152c950ef00c2bfb2baf7eeb785c052e05
Naimul <rjnaimul@gmail.com>
2022-04-10 04:52:34 +0600

    update navbar code

--- js/main.js
+++ js/main.js
@@ -14,3 +14,23 @@ for (i = 0; i < dropdown.length; i++) {
         }
     });
 }
+// document.addEventListener("DOMContentLoaded", function () {
+
+// });
+let desktoptoggle = document.querySelector("#dark-desktop");
+let mobiletoggle = document.querySelector("#dark-mobile");
+desktoptoggle.addEventListener("click", modeSwitch);
+mobiletoggle.addEventListener("click", modeSwitch);
+
+let isLight = false;
+
+function modeSwitch() {
+    isLight = !isLight;
+    let root = document.body;
+
+    isLight ? desktoptoggle.firstElementChild.classList = "fa-solid fa-moon" : desktoptoggle.firstElementChild.classList = "fa-solid fa-sun";
+
+    isLight ? mobiletoggle.firstElementChild.classList = "fa-solid fa-moon" : mobiletoggle.firstElementChild.classList = "fa-solid fa-sun";
+
+    root.classList.toggle("light-theme");
+}
